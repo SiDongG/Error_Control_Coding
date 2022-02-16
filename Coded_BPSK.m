25b93d8b3273d233efad9b3816487556eb8473e4
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-02-15 23:59:36 -0500

    Add files via upload

--- Coded_BPSK.m
+++ Coded_BPSK.m
@@ -5,20 +5,20 @@ k=7;
 n=4;
 Block_Num=5000;
 Bits=randi(0:1,[1,n,Block_Num]);
-G=[1 0 0 0 1 1 0;
+G=[1 0 0 0 1 1 0;      %Hamming Code generator Matrix 
    0 1 0 0 0 1 1;
    0 0 1 0 1 1 1;
    0 0 0 1 1 0 1];
 
-Coded_bits=zeros([1,k,Block_Num]);
+Coded_bits=zeros([1,k,Block_Num]);      %Bits initialization
 for a=1:Block_Num
     Coded_bits(:,:,a)=mod(Bits(:,:,a)*G,2);
 end
 
-Symbols=qammod(Bits,2);
-Coded_Symbols=qammod(Coded_bits,2);
+Symbols=qammod(Bits,2);      %BPSK modulation
+Coded_Symbols=qammod(Coded_bits,2);      %BPSK modulation for coded bits
 
-nr=randn(1,n,Block_Num);
+nr=randn(1,n,Block_Num);      %Noise generation
 ni=randn(1,n,Block_Num);
 Noise=(sqrt(2)/2)*(nr+1i*ni);
 nr=randn(1,k,Block_Num);
@@ -31,11 +31,11 @@ for SNRdb=0:2:10
     Error=0;Coded_error=0;
     SNR=10^(SNRdb/10);
     Symbols2=Symbols+(1/sqrt(SNR))*Noise;
-    Coded_Symbols2=Coded_Symbols+(1/sqrt(SNR))*Noise_coded;
-    Bits_r=qamdemod(Symbols2,2);
-    Coded_bits_r=qamdemod(Coded_Symbols2,2);
-    Corrected_bits=Hamming_correction(Coded_bits_r,Block_Num,k,n);
-    for count=1:Block_Num*n
+    Coded_Symbols2=Coded_Symbols+(1/sqrt(SNR))*Noise_coded;   
+    Bits_r=qamdemod(Symbols2,2);     %BPSK demodulation
+    Coded_bits_r=qamdemod(Coded_Symbols2,2);     %BPSK demodulation for coded bits
+    Corrected_bits=Hamming_correction(Coded_bits_r,Block_Num,k,n);     %Error correction, calling Hamming_correction function
+    for count=1:Block_Num*n      %Compare and calculate bit error rate
         if Bits_r(count)~=Bits(count)
             Error=Error+1;
         end
@@ -47,7 +47,7 @@ for SNRdb=0:2:10
     Coded_ratio(SNRdb/2+1)=Coded_error/(Block_Num*n);
 end
 
-figure()
+figure()     %Plot results
 semilogy(0:2:10,ratio)
 hold on
 semilogy(0:2:10,Coded_ratio)
--- Hamming_correction.m
+++ Hamming_correction.m
@@ -1,16 +1,16 @@
 function Corrected_bits=Hamming_correction(Coded_bits,Block_Num,k,n)
 Corrected_bits=zeros(1,k,Block_Num);
-H=[1 0 0 1 0 1 1;
+H=[1 0 0 1 0 1 1;      %Hamming code parity matrix (Systemic code)
    0 1 0 1 1 1 0;
    0 0 1 0 1 1 1];
-T=[1,0,0,0;
+T=[1,0,0,0;      %Truncating matrix 
     0,1,0,0;
     0,0,1,0;
     0,0,0,1;
     0,0,0,0;
     0,0,0,0;
     0,0,0,0];
-for i=1:Block_Num 
+for i=1:Block_Num     %Multiply received bits with parity matrix, check error and corrects single bit error
     if mod(Coded_bits(:,:,i)*H.',2)==[1,0,1]
         Corrected_bits(:,:,i)=mod(Coded_bits(:,:,i)+[0,0,0,0,0,0,1],2);
     end
@@ -36,7 +36,7 @@ for i=1:Block_Num
         Corrected_bits(:,:,i)=mod(Coded_bits(:,:,i)+[0,0,0,0,0,0,0],2);
     end
 end
-Truncated_bits=zeros(1,n,Block_Num);
+Truncated_bits=zeros(1,n,Block_Num);     %remove appended parity bits 
 for i=1:Block_Num
     Truncated_bits(:,:,i)=Corrected_bits(:,:,i)*T;
 end
